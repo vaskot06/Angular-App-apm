b80b68d167c3672ce8a7e292f2e6f68c7bb41811
unknown <vaskot06@gmail.com>
2020-06-20 14:59:29 +0300

    s

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,72 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
-    
+export class ProductListComponent implements OnInit {
+
     pageTitle: string = 'Product list';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     _listFilter: string;
-    get listFilter(): string{
+    errorMessage: string;
+    get listFilter(): string {
         return this._listFilter;
     }
 
-    set listFilter(value: string ){
+    set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    products: IProduct[];
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('i am on init');
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products
+                this.filteredProducts = this.products;
+            }
+            ,
+            error: err => this.errorMessage = err
+        });
     }
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+
     }
 
-    performFilter(filterBy: string): IProduct[]{
+    performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
-        product.productName.toLocaleLowerCase().indexOf(filterBy) !==-1);
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
-    onRatingClicked(message: string): void{
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product list ' + message;
     }
 
